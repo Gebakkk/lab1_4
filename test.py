9fd495263e67232eee113625038b5d2cc6d5db87
Gebakkk1 <oschepkov23@mail.ru>
2026-01-23 01:34:49 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+from lib import count_common_elements
+
+if __name__ == "__main__":
+    a = [1, 2, 2, 3]
+    b = [2, 3, 4]
+    c = [0, 2, 3]
+    print("Общих элементов:", count_common_elements(a, b, c))
